d7ef3de24593d0ce1de3f9179fd85ae7cffb6dc0
susie.su@ugreen.com <susie.su@ugreen.com>
2025-11-14 10:33:41 +0800

    update  process code

---
+++ src/openllm_func_call_synthesizer/data_process/Cp_data_to_docker.sh
@@ -0,0 +1,14 @@
+# 模型训练
+# mcp_train_1030 整个文件拷贝到  docker里的目录
+
+# cp -rf /data0/work/SusieSu/project/openllm_func_call_synthesizer/data/function_call_for_train_1112/mcp_data_1112_for_train   /data0/work/SusieSu/project/workspace/llama/LLaMA-Factory/data/mcp_train_data
+# cd /data0/work/SusieSu/project/workspace/llama/LLaMA-Factory/data/mcp_train_data/mcp_data_1103_for_train
+# cp -f mcp_train.json mcp_dev.json ../../
+
+python generate_functio_call_format_data.py
+
+python data_process_for_train_pipline.py
+
+cp -rf /data0/work/SusieSu/project/openllm_func_call_synthesizer/data/function_call_for_train_1112_v2/mcp_data_1112_for_train  /data0/work/SusieSu/project/workspace/LLaMA-Factory-main/data/mcp_train_data/
+cd  /data0/work/SusieSu/project/workspace/LLaMA-Factory-main/data/mcp_train_data/mcp_data_1112_for_train
+cp -f mcp_train.json mcp_dev.json ../../
