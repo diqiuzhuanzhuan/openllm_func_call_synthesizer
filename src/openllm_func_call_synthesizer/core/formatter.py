fd48cc729205d0bdba40b3291a87faea77e12a70
Loong Ma <Loong.Ma@ugreen.com>
2025-12-02 17:53:25 +0800

    feat: add query_generate_system_header template loading to formatter.py for improved modularity

--- src/openllm_func_call_synthesizer/core/formatter.py
+++ src/openllm_func_call_synthesizer/core/formatter.py
@@ -11,3 +11,8 @@ with (Path(__file__).parents[0] / Path("templates") / Path("critic_function_call
     # noqa: E501
 
 CRITIC_FUNCTION_CALL_SYSTEM_HEADER = __BASE_CRITIC_FUNCTION_CALL_SYSTEM_HEADER.replace("{context_prompt}", "", 1)
+
+with (Path(__file__).parents[0] / Path("templates") / Path("query_generate_system_header.md")).open("r") as f:
+    __BASE_QUERY_GENERATE_SYSTEM_HEADER = f.read()
+
+QUERY_GENERATE_SYSTEM_HEADER = __BASE_QUERY_GENERATE_SYSTEM_HEADER
